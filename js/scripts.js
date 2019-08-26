3b437dfa592af868538405e2fdc358787603412f
Anita Kemp, Jason Huels and Neha Malhotra <jason@>
2019-08-26 09:52:10 -0700

    add html form and prototype functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
- function destination(name, landmarks, dates, notes) {
+ function destination(name, landmark, dates, notes) {
    this.name = name,
    this.landmark = landmark,
-   this.dates = dates,
-   this.notes = notes,
+   this.dates = [dates],
+   this.notes = [notes]
  }
+destination.prototype.addDates = function(newDate) {
+  this.dates.push(newDate);
+}
 
- 
+destination.prototype.addNotes = function(newNotes) {
+  this.notes.push(newNotes);
+}
+
+function passport() {
+  this.destinations = [];
+}
+
+passport.prototype.addDestination = function(destination) {
+  this.destinations.push(destination);
+}
